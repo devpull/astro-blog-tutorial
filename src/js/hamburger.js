e7007a2d81aa9aa47a9f31c23090a59efa2ceb9a
devpull <devpull@yandex.ru>
2025-05-13 21:47:33 +0300

    Hamburger syntax return

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,9 +1,5 @@
 const hamburger = document.querySelector('.hamburger');
 
-if(!hamburger) {
-  return;
-}
-
 hamburger.addEventListener('click', () => {
   document.querySelector('.nav-links').classList.toggle('expanded');
 });
