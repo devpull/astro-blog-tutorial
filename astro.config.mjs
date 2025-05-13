f1e9dd3749b3e520af81e61861ed91511e41fb17
devpull <devpull@yandex.ru>
2025-05-13 21:43:43 +0300

    Astro config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,7 @@ import { defineConfig } from 'astro/config';
 
 import netlify from '@astrojs/netlify';
 
-import sentry from '@sentry/astro';
-import spotlightjs from '@spotlightjs/astro';
-
 // https://astro.build/config
 export default defineConfig({
   adapter: netlify(),
-  integrations: [sentry(), spotlightjs()]
 });
